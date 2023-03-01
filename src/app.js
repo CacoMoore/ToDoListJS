609b201736ea2cd7c8b867c56b63563c0bb865c9
CacoMoore <caroavello.m@gmail.com>
2023-03-01 16:18:54 +0000

    to do list with JS

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Caro!");
+
+  let addToDoButton = document.getElementById("addToDo");
+  let toDoContainer = document.getElementById("to-do-list");
+  let inputfield = document.getElementById("inputfield");
+
+  addToDoButton.addEventListener("click", function() {
+    var text = document.createElement("li");
+    text.innerText = inputfield.value;
+    toDoContainer.appendChild(text);
+    inputfield.value = "";
+    toDoContainer.addEventListener("dblclick", function() {
+      toDoContainer.removeChild(text);
+    });
+  });
 };
